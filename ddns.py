721c07ded521a135a5db0e4bbd59ca4ea3a37d23
Hunter Read <hunter.read.ca@gmail.com>
2023-05-24 12:48:15 -0700

    Added unit testing

--- ddns.py
+++ ddns.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+import sys
 import logging
 import os
 import requests
@@ -15,7 +16,7 @@ logger.addHandler(hdlr)
 logger.setLevel(logging.INFO)
 
 
-def get_ip(server: str) -> str:
+def get_ip(server: str | None) -> str | None:
     if server is None:
         return None
     try:
@@ -28,38 +29,42 @@ def get_ip(server: str) -> str:
     return None
 
 
-def get_ipv4() -> str:
+def get_ipv4() -> str | None:
     return get_ip(os.environ.get("IPV4_SERVER", "https://ipv4.icanhazip.com"))
 
 
-def get_ipv6() -> str:
+def get_ipv6() -> str | None:
     return get_ip(os.environ.get("IPV6_SERVER", None))
 
 
 def get_subdomain_data(domain: str, subdomains: list, headers: dict) -> dict:
     result: dict = {}
     for subdomain in subdomains:
-        response: requests.Response = requests.get(
-            f"https://api.digitalocean.com/v2/domains/{domain}/records?name={subdomain}",
-            headers=headers,
-        )
-        if response.status_code == 200:
-            json_data: dict = response.json().get("domain_records")
-            a_record: str = None
-            aaaa_record: str = None
-            for record in json_data:
-                if record.get("type") == "A":
-                    a_record = record
-                elif record.get("type") == "AAAA":
-                    aaaa_record = record
-            result[subdomain] = (a_record, aaaa_record)
-        else:
-            result[subdomain] = None
+        try:
+            response: requests.Response = requests.get(
+                f"https://api.digitalocean.com/v2/domains/{domain}/records?name={subdomain}",
+                headers=headers,
+            )
+            if response.status_code == 200:
+                json_data: dict = response.json().get("domain_records")
+                a_record: str | None = None
+                aaaa_record: str | None = None
+                for record in json_data:
+                    if record.get("type") == "A":
+                        a_record = record
+                    elif record.get("type") == "AAAA":
+                        aaaa_record = record
+                result[subdomain] = (a_record, aaaa_record)
+            else:
+                result[subdomain] = None
+        except requests.exceptions.RequestException:
+            logger.error("Unable to get subdomain data for %s", subdomain)
+            raise Exception("API Error")
     return result
 
 
 def update_record(
-    domain: str, subdomain: str, old: dict, new: str, type: str, headers: dict
+    domain: str, subdomain: str, old: dict, new: str | None, type: str, headers: dict
 ) -> bool:
     if new is None:
         return False
@@ -78,10 +83,12 @@ def update_record(
             headers=headers,
         )
         if response.status_code == 201:
-            logger.info("Successfully created %s: %s to %s", type, subdomain, new)
+            logger.info(
+                "Successfully created %s record for %s with ip %s", type, subdomain, new
+            )
             return True
         else:
-            logger.error("Unable to create %s: %s to %s - %s", type, subdomain, new)
+            logger.error("Unable to create %s record for %s", type, subdomain)
 
     elif old.get("data") != new:
         # Update the record
@@ -91,41 +98,58 @@ def update_record(
             headers=headers,
         )
         if response.status_code == 200:
-            logger.info("Successfully updated %s: %s to %s", type, subdomain, new)
+            logger.info(
+                "Successfully updated %s record for %s to %s", type, subdomain, new
+            )
             return True
         else:
-            logger.error("Unable to update %s: %s to %s", type, subdomain, new)
+            logger.error("Unable to update %s record for %s", type, subdomain)
     return False
 
 
 def update_records() -> None:
-    ipv4: str = get_ipv4()
-    ipv6: str = get_ipv6()
+    ipv4: str | None = get_ipv4()
+    ipv6: str | None = get_ipv6()
     if not (ipv4 or ipv6):
         logger.error("Unable to get IP from any server")
         return
 
-    api_key: str = os.environ.get("API_KEY")
+    api_key: str | None = os.environ.get("API_KEY")
     if not api_key:
         logger.critical("API_KEY environment variable not set")
         return
     headers = {"Authorization": "Bearer " + api_key, "Content-Type": "application/json"}
-    domains: list = json.loads(os.environ.get("DOMAINS"))
+    domains: list = json.loads(os.environ.get("DOMAINS", "[]"))
     updated = False
-    
+
     for domain_json in domains:
         domain: str = domain_json.get("domain")
         subdomains: list = domain_json.get("subdomains")
-        current_dns: dict = get_subdomain_data(domain, subdomains, headers)
-        for subdomain in subdomains:
-            if current_dns.get(subdomain) is None:
-                logger.info("Subdomain %s not found in current DNS data", subdomain)
-                continue
-            old_ipv4, old_ipv6 = current_dns.get(subdomain)
-            updated |= update_record(domain, subdomain, old_ipv4, ipv4, "A", headers)
-            updated |= update_record(domain, subdomain, old_ipv6, ipv6, "AAAA", headers)
+        try:
+            current_dns: dict[str, tuple] = get_subdomain_data(
+                domain, subdomains, headers
+            )
+            subdomain: str
+            for subdomain in subdomains:
+                if current_dns.get(subdomain) is None:
+                    logger.info("Subdomain %s not found in current DNS data", subdomain)
+                    continue
+                old_ipv4: dict
+                old_ipv6: dict
+                old_ipv4, old_ipv6 = current_dns.get(subdomain)
+                updated |= update_record(
+                    domain, subdomain, old_ipv4, ipv4, "A", headers
+                )
+                updated |= update_record(
+                    domain, subdomain, old_ipv6, ipv6, "AAAA", headers
+                )
+        except Exception:
+            #  If we can't get the current DNS data, we can't update it. Indicates an issue with the API
+            return
+
     if not updated:
         logger.info("No records were updated")
+
 
 if __name__ == "__main__":
     logger.info("Starting ddns")
---
+++ test_ddns.py
@@ -0,0 +1,230 @@
+import unittest
+from unittest.mock import patch, MagicMock
+import requests
+import os
+import logging
+from io import StringIO
+from ddns import (
+    get_ip,
+    get_ipv4,
+    get_ipv6,
+    get_subdomain_data,
+    update_record,
+)
+
+
+class TestDDNS(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger("ddns")
+        self.logger.setLevel(logging.DEBUG)
+        self.stream = StringIO()
+        self.handler = logging.StreamHandler(self.stream)
+        self.handler.setFormatter(
+            logging.Formatter(
+                "%(levelname)s:%(asctime)s: %(message)s", "%Y-%m-%d %H:%M:%S"
+            )
+        )
+        self.logger.addHandler(self.handler)
+        self.api_key = "test_api_key"
+        self.headers = {
+            "Authorization": "Bearer " + self.api_key,
+            "Content-Type": "application/json",
+        }
+        self.domain = "test.com"
+        self.subdomains = ["www.test.com", "mail.test.com"]
+        self.current_dns = {
+            "www.test.com": (
+                {"id": 1, "type": "A", "name": "www", "data": "1.2.3.4"},
+                {
+                    "id": 2,
+                    "type": "AAAA",
+                    "name": "www",
+                    "data": "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+                },
+            ),
+            "mail.test.com": (
+                {"id": 3, "type": "A", "name": "mail", "data": "1.2.3.5"},
+                None,
+            ),
+        }
+
+    def tearDown(self):
+        self.logger.removeHandler(self.handler)
+        del self.handler
+        del self.logger
+        del self.stream
+
+    def test_get_ip(self):
+        # Test successful response
+        with patch.object(requests, "get") as mock_get:
+            mock_response = MagicMock()
+            mock_response.status_code = 200
+            mock_response.text = "1.2.3.4"
+            mock_get.return_value = mock_response
+            self.assertEqual(get_ip("http://test.com"), "1.2.3.4")
+
+        # Test unsuccessful response
+        with patch.object(requests, "get") as mock_get:
+            mock_get.side_effect = requests.exceptions.RequestException
+            self.assertIsNone(get_ip("http://test.com"))
+
+    def test_get_ipv4(self):
+        # Test successful response
+        with patch.dict(os.environ, {"IPV4_SERVER": "http://test.com"}):
+            with patch.object(requests, "get") as mock_get:
+                mock_response = MagicMock()
+                mock_response.status_code = 200
+                mock_response.text = "1.2.3.4"
+                mock_get.return_value = mock_response
+                self.assertEqual(get_ipv4(), "1.2.3.4")
+
+        # Test unsuccessful response
+        with patch.dict(os.environ, {"IPV4_SERVER": "http://test.com"}):
+            with patch.object(requests, "get") as mock_get:
+                mock_get.side_effect = requests.exceptions.RequestException
+                self.assertIsNone(get_ipv4())
+
+    def test_get_ipv6(self):
+        # Test successful response
+        with patch.dict(os.environ, {"IPV6_SERVER": "http://test.com"}):
+            with patch.object(requests, "get") as mock_get:
+                mock_response = MagicMock()
+                mock_response.status_code = 200
+                mock_response.text = "2001:0db8:85a3:0000:0000:8a2e:0370:7334"
+                mock_get.return_value = mock_response
+                self.assertEqual(get_ipv6(), "2001:0db8:85a3:0000:0000:8a2e:0370:7334")
+
+        # Test unsuccessful response
+        with patch.dict(os.environ, {"IPV6_SERVER": "http://test.com"}):
+            with patch.object(requests, "get") as mock_get:
+                mock_get.side_effect = requests.exceptions.RequestException
+                self.assertIsNone(get_ipv6())
+
+    def test_get_subdomain_data(self):
+        # Test successful response
+        with patch.object(requests, "get") as mock_get:
+            mock_response_www = MagicMock()
+            mock_response_www.status_code = 200
+            mock_response_www.json.return_value = {
+                "domain_records": [
+                    {"id": 1, "type": "A", "name": "www", "data": "1.2.3.4"},
+                    {
+                        "id": 2,
+                        "type": "AAAA",
+                        "name": "www",
+                        "data": "2001:0db8:85a3:0000:0000:8a2e:0370:7334",
+                    },
+                ]
+            }
+
+            mock_response_mail = MagicMock()
+            mock_response_mail.status_code = 200
+            mock_response_mail.json.return_value = {
+                "domain_records": [
+                    {"id": 3, "type": "A", "name": "mail", "data": "1.2.3.5"},
+                ]
+            }
+
+            mock_get.side_effect = [
+                mock_response_www,
+                mock_response_mail,
+            ]
+
+            self.assertEqual(
+                get_subdomain_data(self.domain, self.subdomains, self.headers),
+                self.current_dns,
+            )
+
+        # Test unsuccessful response
+        with patch.object(requests, "get") as mock_get:
+            mock_get.side_effect = requests.exceptions.RequestException
+            with self.assertRaises(Exception) as context:
+                get_subdomain_data(self.domain, self.subdomains, self.headers)
+            self.assertEqual(str(context.exception), "API Error")
+
+    def test_update_record(self):
+        # Test successful create
+        with patch.object(requests, "post") as mock_post:
+            mock_response = MagicMock()
+            mock_response.status_code = 201
+            mock_post.return_value = mock_response
+            self.assertTrue(
+                update_record(
+                    self.domain, "www.test.com", None, "1.2.3.4", "A", self.headers
+                )
+            )
+            self.assertIn(
+                "Successfully created A record for www.test.com with ip 1.2.3.4",
+                self.stream.getvalue().strip(),
+            )
+
+        # Test unsuccessful create
+        with patch.object(requests, "post") as mock_post:
+            mock_response = MagicMock()
+            mock_response.status_code = 400
+            mock_post.return_value = mock_response
+            self.assertFalse(
+                update_record(
+                    self.domain, "www.test.com", None, "1.2.3.4", "A", self.headers
+                )
+            )
+            self.assertIn(
+                "Unable to create A record for www.test.com",
+                self.stream.getvalue().strip(),
+            )
+
+        # Test successful update
+        with patch.object(requests, "patch") as mock_patch:
+            mock_response = MagicMock()
+            mock_response.status_code = 200
+            mock_patch.return_value = mock_response
+            self.assertTrue(
+                update_record(
+                    self.domain,
+                    "www.test.com",
+                    self.current_dns["www.test.com"][0],
+                    "1.2.3.5",
+                    "A",
+                    self.headers,
+                )
+            )
+            self.assertIn(
+                "Successfully updated A record for www.test.com to 1.2.3.5",
+                self.stream.getvalue().strip(),
+            )
+
+        # Test unsuccessful update
+        with patch.object(requests, "patch") as mock_patch:
+            mock_response = MagicMock()
+            mock_response.status_code = 400
+            mock_patch.return_value = mock_response
+            self.assertFalse(
+                update_record(
+                    self.domain,
+                    "www.test.com",
+                    self.current_dns["www.test.com"][0],
+                    "1.2.3.5",
+                    "A",
+                    self.headers,
+                )
+            )
+            self.assertIn(
+                "Unable to update A record for www.test.com",
+                self.stream.getvalue().strip(),
+            )
+
+        # Test no update
+        self.assertFalse(
+            update_record(
+                self.domain,
+                "www.test.com",
+                self.current_dns["www.test.com"][0],
+                "1.2.3.4",
+                "A",
+                self.headers,
+            )
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
